f6b49c370c920adbbaff86b9248504e08cd82e73
halleranna <86572050+halleranna@users.noreply.github.com>
2024-05-01 18:39:03 -0400

    Update User.java

--- src/personalityTest/User.java
+++ src/personalityTest/User.java
@@ -53,7 +53,97 @@ public class User {
 	
 	//getters and setters
 	public String getName() {
+		re
+package personalityTest;
+
+import java.util.ArrayList;
+
+/* 
+ * Project: Spongebob Personality Test
+ * Author: Anna Haller
+ * Due Date: 5/1/24
+ * 
+ * User class stores information about the user including
+ * name, answers to test questions, and personality type.
+ * 
+ */
+
+public class User {
+
+	//instance variables
+	private String name;
+	private ArrayList answers;
+	private String personality;
+	
+	//constructor
+	private User(String name) {
+		this.name = name;
+		this.answers = new ArrayList();
+		this.personality = "";
+	}
+	
+	//empty constructor
+	private User() {
+		this.name = "User";
+		this.answers = new ArrayList();
+		this.personality = "";
+	}
+
+	//greetUser greet the user using the name attribute
+	private String greetUser() {
+		return "Hello, " + this.name;
+	}
+
+	
+	//tabulatePersonality uses the answers ArrayList
+	//to determine which personality the user fits
+	private String decidePersonality() {
+		int spongebob = 0, sandy = 0, squidward = 0;
+		for(int i = 0; i < answers.size(); i++) {
+			if(spongebob > sandy && spongebob > squidward) {
+				this.personality = "Spongebob";
+			} else if (sandy > spongebob && sandy > squidward) {
+				this.personality = "Sandy";
+			} else if (squidward > spongebob && squidward > sandy) {
+				this.personality = "Squidward";
+			} else if (spongebob == sandy && spongebob > squidward) {
+				this.personality = "Patrick";
+			} else if (spongebob == squidward && spongebob > sandy) {
+				this.personality = "Mr. Krabs";
+			} else if(sandy == squidward && sandy > spongebob) {
+				this.personality = "Plankton"; 
+			}
+		}
+		return this.personality;
+	}
+	
+	//getters and setters
+	public String getName() {
 		return this.name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public ArrayList getAnswers() {
+		return this.answers;
+	}
+	
+	public void setAnswers(ArrayList answers) {
+		this.answers = answers;
+	}
+	
+	public String getPersonality() {
+		return this.personality;
+	}
+	
+	public void setPersonality(String alignment) {
+		this.personality = personality;
+	}
+	
+}
+turn this.name;
 	}
 	
 	public void setName(String name) {
