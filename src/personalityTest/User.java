bfe4412be01ebe259e8aed70d1c36632ecbf8e13
Jay <92336716+jaredtmelton@users.noreply.github.com>
2024-05-01 20:09:08 -0400

    Update User.java

--- src/personalityTest/User.java
+++ src/personalityTest/User.java
@@ -2,6 +2,8 @@
 package personalityTest;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 /* 
  * Project: Moral Alignment Personality Test
@@ -15,155 +17,87 @@ import java.util.ArrayList;
 
 public class User {
 
-	//instance variables
-	private String name;
-	private ArrayList answers;
-	private String alignment;
-	
-	//constructor
-	private User(String name) {
-		this.name = name;
-		this.answers = new ArrayList();
-		this.alignment = "";
-	}
-	
-	//empty constructor
-	private User() {
-		this.name = "User";
-		this.answers = new ArrayList();
-		this.alignment = "";
-	}
+    private String name;
+    private ArrayList<String> answers;
+    private String alignment; 
 
-	//greetUser greet the user using the name attribute
-	private String greetUser() {
-		return "";
-	}
-	
-	//recordAnswer adds a user's answer to the
-	//answers ArrayList
-	private void recordAnswer(String answer) {
-		
-	}
-	
-	//tabulateAlignment uses the answers ArrayList
-	//to determine the user's alignment
-	private String tabulateAlignment() {
-		return "";
-	}
-	
-	//getters and setters
-	public String getName() {
-		re
-package personalityTest;
+    // Constructor to initialize the User with a name.
+    public User(String name) {
+        this.name = name;
+        this.answers = new ArrayList<>();
+        this.alignment = "";
+    }
 
-import java.util.ArrayList;
+    // Method to record an answer into the user's list of answers.
+    public void recordAnswer(String answer) {
+        this.answers.add(answer);
+    }
 
-/* 
- * Project: Spongebob Personality Test
- * Author: Anna Haller
- * Due Date: 5/1/24
- * 
- * User class stores information about the user including
- * name, answers to test questions, and personality type.
- * 
- */
+    // Method created to analyze the user's answers and determine the most fitting character result
+    public String tabulateAlignment() {
+        // Making a mapping for the characters
+        Map<String, Integer> characterScores = new HashMap<>();
+        characterScores.put("SpongeBob", 0);
+        characterScores.put("Patrick", 0);
+        characterScores.put("Squidward", 0);
+        characterScores.put("Mr. Krabs", 0);
+        characterScores.put("Sandy", 0);
+        characterScores.put("Plankton", 0); // Including Plankton for additional options.
 
-public class User {
+        // Loop through all answers and increment the score for the appropriate character based on the answer.
+        for (String answer : answers) {
+            switch (answer) {
+                case "I keep smiling and stay optimistic":
+                case "Jellyfishing":
+                case "Friends and fun":
+                case "Krabby Patty":
+                case "Frying up food at a fast-food restaurant":
+                case "Snail":
+                    characterScores.put("SpongeBob", characterScores.get("SpongeBob") + 1);
+                    break;
+                case "I hardly notice":
+                case "Sleeping":
+                case "Loyal but clueless":
+                case "Ice Cream":
+                case "Being a star":
+                case "Worm":
+                    characterScores.put("Patrick", characterScores.get("Patrick") + 1);
+                    break;
+                case "I get grumpy and complain":
+                case "Playing music":
+                case "Cynical and sarcastic":
+                case "Anything I don't have to cook":
+                case "Being an artist":
+                case "No pets, too much hassle":
+                    characterScores.put("Squidward", characterScores.get("Squidward") + 1);
+                    break;
+                case "Counting my money":
+                case "Money and power":
+                case "Greedy and cheap":
+                case "Managing a business":
+                    characterScores.put("Mr. Krabs", characterScores.get("Mr. Krabs") + 1);
+                    break;
+                case "Practicing karate":
+                case "Knowledge and inventing":
+                case "Inventive and smart":
+                case "A protein-packed snack":
+                case "Inventing new gadgets":
+                case "A high-energy squirrel":
+                    characterScores.put("Sandy", characterScores.get("Sandy") + 1);
+                    break;
+                case "Scheming":
+                case "Small but mighty":
+                case "Mastermind":
+                    characterScores.put("Plankton", characterScores.get("Plankton") + 1);
+                    break;
+            }
+        }
 
-	//instance variables
-	private String name;
-	private ArrayList answers;
-	private String personality;
-	
-	//constructor
-	private User(String name) {
-		this.name = name;
-		this.answers = new ArrayList();
-		this.personality = "";
-	}
-	
-	//empty constructor
-	private User() {
-		this.name = "User";
-		this.answers = new ArrayList();
-		this.personality = "";
-	}
-
-	//greetUser greet the user using the name attribute
-	private String greetUser() {
-		return "Hello, " + this.name;
-	}
-
-	
-	//tabulatePersonality uses the answers ArrayList
-	//to determine which personality the user fits
-	private String decidePersonality() {
-		int spongebob = 0, sandy = 0, squidward = 0;
-		for(int i = 0; i < answers.size(); i++) {
-			if(spongebob > sandy && spongebob > squidward) {
-				this.personality = "Spongebob";
-			} else if (sandy > spongebob && sandy > squidward) {
-				this.personality = "Sandy";
-			} else if (squidward > spongebob && squidward > sandy) {
-				this.personality = "Squidward";
-			} else if (spongebob == sandy && spongebob > squidward) {
-				this.personality = "Patrick";
-			} else if (spongebob == squidward && spongebob > sandy) {
-				this.personality = "Mr. Krabs";
-			} else if(sandy == squidward && sandy > spongebob) {
-				this.personality = "Plankton"; 
-			}
-		}
-		return this.personality;
-	}
-	
-	//getters and setters
-	public String getName() {
-		return this.name;
-	}
-	
-	public void setName(String name) {
-		this.name = name;
-	}
-	
-	public ArrayList getAnswers() {
-		return this.answers;
-	}
-	
-	public void setAnswers(ArrayList answers) {
-		this.answers = answers;
-	}
-	
-	public String getPersonality() {
-		return this.personality;
-	}
-	
-	public void setPersonality(String alignment) {
-		this.personality = personality;
-	}
-	
+        // Determine which character from SpongeBob has the highest score. Return their results as the user's character alignment
+        return characterScores.entrySet().stream()
+                               .max(Map.Entry.comparingByValue())
+                               .map(Map.Entry::getKey)
+                               .orElse("Unknown");
+    }
 }
-turn this.name;
-	}
-	
-	public void setName(String name) {
-		this.name = name;
-	}
-	
-	public ArrayList getAnswers() {
-		return this.answers;
-	}
-	
-	public void setAnswers(ArrayList answers) {
-		this.answers = answers;
-	}
-	
-	public String getAlignment() {
-		return this.alignment;
-	}
-	
-	public void setAlignment(String alignment) {
-		this.alignment = alignment;
-	}
-	
-}
+
